555f5443b67178c79c71ae9a5a34516c72ac8ed6
Evgeny Tkach <1eluzive1@gmail.com>
2021-01-11 09:30:47 +0300

    refactored

--- src/Introduction/Vol1/Vol1VarA.java
+++ src/Introduction/Vol1/Vol1VarA.java
@@ -3,20 +3,20 @@ package Introduction.Vol1;
 import java.util.Scanner;
 
 public class Vol1VarA {
-    private static String name = "";
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        first();
-        second(args);
-        third();
-        fourth();
-        fifth();
-        sixth();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
     }
 
     //1. Приветствовать любого пользователя при вводе его имени через командную строку.
-    public static void first() {
+    public static void task1() {
+        String name = "";
         //1
         System.out.print("Your name:");
         try {
@@ -24,42 +24,31 @@ public class Vol1VarA {
         } catch (Exception exception) {
             System.out.println("Ошибка ввода");
         }
-        System.out.println("Hello," + name);
+        printHelloName(name);
 
         System.out.println("/////////////");
     }
 
-    public static void reverse(String[] string) {
-        String tmp = "";
-        for (int i = 0; i < string.length / 2; i++) {
-            tmp = string[i];
-            string[i] = string[string.length - i - 1];
-            string[string.length - i - 1] = tmp;
-        }
-        //return string;
+    public static void printHelloName(String name){
+        System.out.println("Hello," + name);
     }
 
-    // 2. Отобразить в окне консоли аргументы командной строки в обратном
-    //порядке.
-    public static void second(String[] args) {
 
-        //args = reverse(args);
-        for (String arg : args) {
-            System.out.println(arg);
-        }
+    // 2. Отобразить массив строк в обратном порядке.
+    public static void task2() {
 
-
-        //2.1 Просто решение задачи с преобразованием в обратный массив
-        String[] stringsToTurning = new String[10];
-        stringsToTurning[0] = "";
-        for (int i = 0; i < stringsToTurning.length; i++) {
-            stringsToTurning[i] = Integer.toString(i);
-            System.out.println(stringsToTurning[i]);
-        }
-        System.out.println("Reversed:");
-        reverse(stringsToTurning);
-        for (String str : stringsToTurning) {
+        String[] stringsToTurning = {"first", "second"};
+        System.out.println("Array of strings:");
+        for (String str:stringsToTurning
+             ) {
             System.out.println(str);
+
+        }
+        System.out.println();
+
+        System.out.println("Reversed :");
+        for (int i = stringsToTurning.length - 1; i >= 0; i--) {
+            System.out.println(stringsToTurning[i]);
         }
 
         System.out.println("/////////////");
@@ -67,65 +56,77 @@ public class Vol1VarA {
 
     //3. Вывести заданное количество случайных чисел с переходом и без перехода
     //        на новую строку.
-    public static void third() {
-        int amount = 0;
-        System.out.print("Введите количество случайных чисел: ");
-        try {
-            amount = scanner.nextInt();
-        } catch (Exception exception) {
-            System.out.println(exception.fillInStackTrace().toString());
-            System.out.println("Введенные данные некорректны");
-        }
+    public static void task3() {
+        final int amountRndNumbers = 10;
+        final int maxRandomValue = 10;
+        final boolean onlyPositive = true;
 
-        StringBuilder resultInOneLine = new StringBuilder();
-        int[] numbers = new int[amount];
-        for (int i = 0; i < amount; i++) {
-            numbers[i] = (int) (Math.random() * 10);
-            resultInOneLine.append(Integer.toString(numbers[i]));
-            System.out.println(numbers[i]);
-        }
+        int[] numbers = getRandomIntArray(amountRndNumbers, maxRandomValue, onlyPositive);
 
         System.out.println("Вывод без перехода на новую строку:");
+        System.out.println(arrayIntToString(numbers));
 
-        /*for (int i=0; i<amount;i++){
-            System.out.print(numbers[i]);
-        }*/
-        System.out.println(resultInOneLine);//такая реализации менее затратная
-        System.out.println("\n/////////////");
+
+        System.out.println("Вывод c переходом на новую строку:");
+        printArrayNumbers(numbers);
+        System.out.println("/////////////");
+    }
+
+    public static String arrayIntToString(int[] numbers){
+        StringBuilder str = new StringBuilder();
+
+        for (Integer num: numbers
+             ) {
+            str.append(num.toString()).append(" "); //или можно использовать метод из Arrays, вместо всего этого метода
+        }
+        return str.toString();
+    }
+
+    public static int[] getRandomIntArray(int length, int maxRandomValue, boolean onlyPositive){
+
+        int[] array = new int[length];
+        int varForNegativeNumbers;
+        if (onlyPositive)  varForNegativeNumbers = 0;
+            else varForNegativeNumbers = length/2;
+        for (int i = 0; i < length; i++) {
+            array[i] = (int) (Math.random() * maxRandomValue) - varForNegativeNumbers;
+        }
+
+        return array;
+    }
+
+    public static void printArrayNumbers(int[] numbers){ //можно переделать в дженерик, надо тогда надо менять работу с массивом
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 
     //4. Ввести пароль из командной строки и сравнить его со строкой-образцом.
-    public static void fourth() {
+    public static void task4() {
         System.out.println("Введите - АБВГД");
         String str = scanner.next();
-        String strExample = "АБВГД";
+        final String strExample = "АБВГД";
         if (str.equals(strExample))
             System.out.println("Введенная строка совпадает со строкой образцом АБВГД");
         else
             System.out.println("Введенная строка НЕ совпадает со строкой образцом АБВГД");
-
     }
 
-    //5. Ввести целые числа как аргументы командной строки,
+
+    //5. Ввести целые числа
     // подсчитать их суммы (произведения) и вывести результат на консоль.
-    public static void fifth() {
+    public static void task5() {
         try {
             System.out.println("Введите первое число: ");
             int number1 = scanner.nextInt();
-            System.out.println("Введите второго число: ");
+            System.out.println("Введите второе число: ");
             int number2 = scanner.nextInt();
-            System.out.println("Сумма этих чисел: " + Integer.toString(number1 + number2));
+            System.out.println("Сумма этих чисел: " + number1 + number2);
         } catch (Exception exception) {
             System.out.println(exception.fillInStackTrace().toString());
             System.out.println("Ошибка ввода данных");
         }
-
     }
 
-    //6. Вывести фамилию разработчика, дату и время получения задания, а также
-    //дату и время сдачи задания.
-    public static void sixth() {
-        System.out.println("Ткач Евгений Михайлович");
-        System.out.println("Начал 13/12/20 н.э, доделал 16/12/20 н.э. 2 подхода по 20-30 минут");
-    }
+
 }
